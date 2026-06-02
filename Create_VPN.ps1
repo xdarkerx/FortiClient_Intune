f8d9384d715867933a49d6f26d85b20e2a3f4483
Vinicius <62823439+xdarkerx@users.noreply.github.com>
2026-06-02 07:48:10 -0300

    Alteração do código

--- Create_VPN.ps1
+++ Create_VPN.ps1
@@ -1,4 +1,8 @@
-# Log
+# tarefa no task scheduler
+$taskName = "FortiClient_VPN_Configurator"
+$taskDescription = "Executa o script Configure_VPN.ps1 na inicializaÃ§Ã£o do sistema."
+
+#log
 $logDir = "C:\IntuneLogs\FortiClient\Create_VPN_Logs"
 $logFile = "$logDir\log_$(Get-Date -Format 'yyyy-MM-dd').txt"
 
@@ -16,44 +20,38 @@ function Write-Log {
     Add-Content -Path $logFile -Value "$logMessage"
 }
 
-#mover o arquivo de configuração
+#mover o arquivo de configuraÃ§Ã£o
 function Move-ConfigFile {
     $sourcePath = Join-Path -Path $PSScriptRoot -ChildPath "Configure_VPN.ps1"
     $destinationPath = "C:\IntuneFiles\FortiClient\Configure_VPN.ps1"
-    $destinationDir = [System.IO.Path]::GetDirectoryName($destinationPath)  # Pega o diretório de destino
+    $destinationDir = [System.IO.Path]::GetDirectoryName($destinationPath)  # Pega o diretÃ³rio de destino
 
     if (Test-Path $sourcePath) {
-        Write-Log "Movendo o arquivo de configuração para $destinationPath"
+        Write-Log "Movendo o arquivo de configuraÃ§Ã£o para $destinationPath"
         
         
         if (-not (Test-Path $destinationDir)) {
-            Write-Log "Criando diretório de destino: $destinationDir"
+            Write-Log "Criando diretÃ³rio de destino: $destinationDir"
             New-Item -Path $destinationDir -ItemType Directory -Force
         }
 
-        # Ler o conteúdo do arquivo de origem com a codificação UTF-8
         $content = Get-Content -Path $sourcePath -Encoding UTF8
         
         Set-Content -Path $destinationPath -Value $content -Encoding UTF8
         
-        Write-Log "Arquivo de configuração movido com sucesso."
+        Write-Log "Arquivo de configuraÃ§Ã£o movido com sucesso."
     } else {
-        Write-Log "Arquivo de configuração não encontrado no caminho original: $sourcePath"
+        Write-Log "Arquivo de configuraÃ§Ã£o nÃ£o encontrado no caminho original: $sourcePath"
         exit 1
     }
 }
 
 Move-ConfigFile
 
-# Nome da Tarefa
-$taskName = "FortiClient_VPN_Configurator"
-$taskDescription = "Executa o script Configure_VPN.ps1 na inicialização do sistema."
-
 $existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
 if ($existingTask) {
-    Write-Log "A tarefa '$taskName' já existe. Nenhuma nova tarefa será criada."
+    Write-Log "A tarefa '$taskName' jÃ¡ existe. Nenhuma nova tarefa serÃ¡ criada."
 } else {
-    # Ação para rodar o script Configure_VPN.ps1
     $taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -File C:\IntuneFiles\FortiClient\Configure_VPN.ps1"
 
     $taskTrigger = New-ScheduledTaskTrigger -AtStartup
@@ -61,9 +59,9 @@ if ($existingTask) {
     $taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
 
     Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Principal $taskPrincipal -TaskName $taskName -Description $taskDescription
-    Write-Log "Tarefa agendada para execução do script Configure_VPN.ps1 na inicialização criada com sucesso."
+    Write-Log "Tarefa agendada para execuÃ§Ã£o do script Configure_VPN.ps1 na inicializaÃ§Ã£o criada com sucesso."
 }
 
-Write-Log "Iniciando a execução imediata da tarefa agendada '$taskName'."
+Write-Log "Iniciando a execuÃ§Ã£o imediata da tarefa agendada '$taskName'."
 Start-ScheduledTask -TaskName $taskName
 Write-Log "Tarefa '$taskName' executada com sucesso."
